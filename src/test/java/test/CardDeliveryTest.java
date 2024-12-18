74f00b430ee3d11a25a8465984dbc7be335379d3
KuprKazik <kuprkazik94@gmail.com>
2024-12-18 17:42:28 +0300

    Allure added

--- src/test/java/test/CardDeliveryTest.java
+++ src/test/java/test/CardDeliveryTest.java
@@ -1,9 +1,9 @@
 package test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import data.DataGenerator;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 
@@ -16,8 +16,19 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
+   @BeforeAll
+   static void setUpAll() {
+       SelenideLogger.addListener("allure", new AllureSelenide());
+   }
+
+   @AfterAll
+   static void tearDownAll() {
+       SelenideLogger.removeListener("allure");
+   }
+
     @BeforeEach
     void setup() {
+
         open("http://localhost:9999");
     }
 
